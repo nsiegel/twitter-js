24c7ea32bbadc62cb3d4ab339166036420e910bf
Nicole <nsiegel2@gmail.com>
2015-11-10 15:28:15 -0500

    loggin middleware

--- app.js
+++ app.js
@@ -1,8 +1,29 @@
 var express = require('express');
+var fs = require('fs');
+var morgan = require('morgan');
 var app = express();
 
+// create a write stream (in append mode)
+var accessLogStream = fs.createWriteStream(__dirname + '/access.log', {flags: 'a'})
+
+// setup the logger
+app.use(morgan('combined', {stream: accessLogStream}))
+
+app.get('/', function (req, res, next) {
+  // console.log(req.route);
+  console.log(accessLogStream);
+  console.log('GET\n');
+  next();
+});
+
+app.post('/', function (req, res, next) {
+  // console.log(req.route);
+  console.log('POST\n');
+  next();
+});
+
 app.get("/", function(req, res) {
-	res.send('Welcome');
+	console.log('Welcome');
 });
 
 var port = 3000;
