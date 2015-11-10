a19f77c484a750f552bd337f1580cfa594cba7ba
Nicole <nsiegel2@gmail.com>
2015-11-10 17:52:59 -0500

    rendering the swig template from app.js

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ var morgan = require('morgan');
 var swig = require('swig');
 
 var app = express();
+var people = [{name: 'Full'}, {name: 'Stacker'}, {name: 'Son'}];
 
 // create a write stream (in append mode)
 var accessLogStream = fs.createWriteStream(__dirname + '/access.log', {flags: 'a'})
@@ -11,12 +12,23 @@ var accessLogStream = fs.createWriteStream(__dirname + '/access.log', {flags: 'a
 // setup the logger
 app.use(morgan('combined', {stream: accessLogStream}))
 
+//SWIG STUFF
+
 app.engine('html', swig.renderFile);
 
 app.set('view engine', 'html');
 app.set('views', __dirname + '/views');
 
 swig.setDefaults({ cache: false });
+
+app.get('/', function (req, res, next) {
+  res.render('index', {title: 'Hall of Fame', people: people},function(err,html){
+    if (err) return next(err);
+    res.send(html);
+  });
+});
+
+//END OF SWIG STUFF
 
 app.get('/', function (req, res, next) {
   // console.log(req.route);
