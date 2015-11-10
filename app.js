926fc4d777681210246e6c754595f2c715407cbb
Nicole <nsiegel2@gmail.com>
2015-11-10 13:06:35 -0500

    modified app.js to set up a server and put out welcome msg

--- app.js
+++ app.js
@@ -1,2 +1,11 @@
 var express = require('express');
 var app = express();
+
+app.get("/", function(req, res) {
+	res.send('Welcome');
+});
+
+var port = 3000;
+app.listen(port, function() {
+	console.log("listening to port "+ port);
+});
